9156900d98652bc029d6dfa01bfd2f5c0423251d
Jackie Wu <jackielwu@gmail.com>
2017-07-26 17:29:30 -0400

    changed sscanf \n to \v

--- search-engine.cpp
+++ search-engine.cpp
@@ -117,7 +117,7 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
   		int index;
   		char url[200];
   		char desc[600];
-  		sscanf(parse,"%d %s\n%s",&index,url,desc);
+  		sscanf(parse,"%d %s\v%s",&index,url,desc);
   		printf("%d %s\n",index,desc);
   		/*char *index = strtok(line," ");
   		//printf("%s",word);
