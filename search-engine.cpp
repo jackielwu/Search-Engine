6ee57ecf79b411d41206ca8921134f9766aa8ed2
Jackie Wu <jackielwu@gmail.com>
2017-07-27 12:32:55 -0400

    t

--- search-engine.cpp
+++ search-engine.cpp
@@ -83,7 +83,7 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
 	URLRecordList *num = new URLRecordList();
 	URLRecordList *e=num;
 	char *word;
-	printf("load words\n");
+	//printf("load words\n");
 	while(*buffer!='\0')
 	{
 		if(onword) {
